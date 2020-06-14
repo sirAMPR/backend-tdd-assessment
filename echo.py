ab9200838731ee7780bdd9ce6f0bd12389d0bb4b
Drew Radcliff <sirampr@gmail.com>
2020-06-14 19:01:03 -0400

    add create_paser

--- echo.py
+++ echo.py
@@ -2,22 +2,29 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility."""
 
-__author__ = "???"
+__author__ = "aradcliff"
 
 
 import sys
+import argparse
 
 
 def create_parser():
     """Returns an instance of argparse.ArgumentParser"""
-    # your code here
-    return
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--upper', '-u', help='convert text to uppercase')
+    parser.add_argument('--lower', '-l', help='convert text to lowercase')
+    parser.add_argument('--title', '-t', help='convert text to titlecase')
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    # your code here
-    return
+    parser = create_parser()
+    ns = parser.parse_args(args)
+
+    if ns.upper:
+        print(ns.upper.upper())
 
 
 if __name__ == '__main__':
